058c5359e488ceb3460496fd03c0146cf8f7249c
Matt Delashaw <mattdelashaw@gmail.com>
2015-12-23 18:04:25 -0600

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,11 @@
+/*
+  
+   Author:matt delashaw
+   Date:5/2/15
+
+*/
+
+var adURLs = [];
+adURLs[0] = "http://www.cheaperthandirt.com/";
+adURLs[1] = "http://www.historymaker.com/";
+adURLs[2] = "http://www.paradisesiberians.com/";
