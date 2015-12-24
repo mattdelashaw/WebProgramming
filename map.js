3c511b9a36d2118eb8a14d2d065b2ef4753f6167
Matt Delashaw <mattdelashaw@gmail.com>
2015-12-23 18:05:50 -0600

    Create map.js

---
+++ map.js
@@ -0,0 +1,19 @@
+/*
+
+	author: matt delashaw
+	date: 5/2/15
+
+*/
+
+
+var title = new Array();
+title[0] = "Callisburg";
+title[1] = "Home";
+title[2] = "Cheaper Than Dirt";
+title[3] = "Paradise Siberians";
+
+var summary = new Array();
+summary[0] = "This is where I'm from and spend my time outdoors.";
+summary[1] = "This is where my house is roundabouts.";
+summary[2] = "This is where I pick up some gun parts if they're not online.";
+summary[3] = "We bought our Husky from a breeder down in Sante Fe. It's way down by houston."
